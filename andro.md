9eca417c9f81c292483f43a7781158b32adf415c
Vojtěch Tranta <vojta.tranta@gmail.com>
2016-10-18 23:34:03 +0200

    using v7; s3 updater feed url

---
+++ andro.md
@@ -0,0 +1,6 @@
+## Using 'builder v7.14.1' 
+
+https://blog.avocode.com/blog/get-that-damn-windows-auto-update-working-on-electron
+
+https://github.com/avocode/electron-windows-autoupdate
+
---
+++ index.js
@@ -0,0 +1,38 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+
+let mainWindow
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({ width: 800, height: 600, visible: true})
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+const squirrelCommand = process.argv[1]
+  switch (squirrelCommand) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+    case '--squirrel-obsolete':
+      app.quit()
+      return true
+  }
